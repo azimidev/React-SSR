9c16879ea48bae3720d1cf723d3aca8a83f82919
Hazz Azimi <hassanazimi@users.noreply.github.com>
2019-09-09 22:12:57 +0100

    Video 9

--- server/build/bundle.js
+++ server/build/bundle.js
@@ -6935,10 +6935,14 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
 
 var app = (0, _express2.default)();
 
+app.use(_express2.default.static('public'));
+
 app.get('/', function (req, res) {
 	var content = (0, _server.renderToString)(_react2.default.createElement(_Home2.default, null));
 
-	res.send(content);
+	var html = "\n\t\t<html>\n\t\t\t<head></head>\n\t\t\t<body>\n\t\t\t\t<div>" + content + "</div>\n\t\t\t\t<script src=\"bundle.js\"></script>\n\t\t\t</body>\n\t\t</html>\n\t";
+
+	res.send(html);
 });
 
 app.listen(3000, function () {

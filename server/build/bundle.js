830e2c6b9c6a909bdbf75ba31553a4cc9e43b052
Hazz Azimi <hassanazimi@users.noreply.github.com>
2019-09-09 21:24:38 +0100

    Video 7

--- server/build/bundle.js
+++ server/build/bundle.js
@@ -6917,14 +6917,26 @@ if (process.env.NODE_ENV === 'production') {
 "use strict";
 
 
-var express = __webpack_require__(52);
-var React = __webpack_require__(16);
-var renderToString = __webpack_require__(113).renderToString;
-var Home = __webpack_require__(124).default;
-var app = express();
+var _express = __webpack_require__(52);
+
+var _express2 = _interopRequireDefault(_express);
+
+var _react = __webpack_require__(16);
+
+var _react2 = _interopRequireDefault(_react);
+
+var _server = __webpack_require__(113);
+
+var _Home = __webpack_require__(!(function webpackMissingModule() { var e = new Error("Cannot find module \"Home\""); e.code = 'MODULE_NOT_FOUND'; throw e; }()));
+
+var _Home2 = _interopRequireDefault(_Home);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var app = (0, _express2.default)();
 
 app.get('/', function (req, res) {
-	var content = renderToString(React.createElement(Home, null));
+	var content = (0, _server.renderToString)(_react2.default.createElement(_Home2.default, null));
 
 	res.send(content);
 });
@@ -22682,31 +22694,6 @@ function camelize(string) {
 }
 
 module.exports = camelize;
-
-/***/ }),
-/* 124 */
-/***/ (function(module, exports, __webpack_require__) {
-
-"use strict";
-
-
-Object.defineProperty(exports, "__esModule", {
-    value: true
-});
-
-var _react = __webpack_require__(16);
-
-var _react2 = _interopRequireDefault(_react);
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-
-exports.default = function () {
-    return _react2.default.createElement(
-        "div",
-        null,
-        "Home"
-    );
-};
 
 /***/ })
 /******/ ]);

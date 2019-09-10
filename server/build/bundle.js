9d7e9bc17fd8c898075cae38497eee4c5e6a5711
Hazz Azimi <hassanazimi@users.noreply.github.com>
2019-09-10 13:28:40 +0100

    Video 14

--- server/build/bundle.js
+++ server/build/bundle.js
@@ -80,15 +80,9 @@ var _express = __webpack_require__(2);
 
 var _express2 = _interopRequireDefault(_express);
 
-var _react = __webpack_require__(0);
+var _renderer = __webpack_require__(5);
 
-var _react2 = _interopRequireDefault(_react);
-
-var _server = __webpack_require__(3);
-
-var _Home = __webpack_require__(4);
-
-var _Home2 = _interopRequireDefault(_Home);
+var _renderer2 = _interopRequireDefault(_renderer);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
@@ -97,15 +91,11 @@ var app = (0, _express2.default)();
 app.use(_express2.default.static('public'));
 
 app.get('/', function (req, res) {
-	var content = (0, _server.renderToString)(_react2.default.createElement(_Home2.default, null));
-
-	var html = "\n\t\t<html>\n\t\t\t<head></head>\n\t\t\t<body>\n\t\t\t\t<div id=\"root\">" + content + "</div>\n\t\t\t\t<script src=\"bundle.js\"></script>\n\t\t\t</body>\n\t\t</html>\n\t";
-
-	res.send(html);
+  return res.send((0, _renderer2.default)());
 });
 
 app.listen(3000, function () {
-	return console.log('Listening on port 3000');
+  return console.log('Listening on port 3000');
 });
 
 /***/ }),
@@ -154,6 +144,35 @@ exports.default = function () {
             "Click Me"
         )
     );
+};
+
+/***/ }),
+/* 5 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+Object.defineProperty(exports, "__esModule", {
+	value: true
+});
+
+var _react = __webpack_require__(0);
+
+var _react2 = _interopRequireDefault(_react);
+
+var _server = __webpack_require__(3);
+
+var _Home = __webpack_require__(4);
+
+var _Home2 = _interopRequireDefault(_Home);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+exports.default = function () {
+	var content = (0, _server.renderToString)(_react2.default.createElement(_Home2.default, null));
+
+	return "\n\t\t<html>\n\t\t\t<head></head>\n\t\t\t<body>\n\t\t\t\t<div id=\"root\">" + content + "</div>\n\t\t\t\t<script src=\"bundle.js\"></script>\n\t\t\t</body>\n\t\t</html>\n\t";
 };
 
 /***/ })

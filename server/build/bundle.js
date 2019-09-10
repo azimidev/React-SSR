29f595ef6d885898b7faf6a3c5a6a00b117b1025
Hazz Azimi <hassanazimi@users.noreply.github.com>
2019-09-10 14:06:58 +0100

    Video 16

--- server/build/bundle.js
+++ server/build/bundle.js
@@ -91,7 +91,7 @@ var app = (0, _express2.default)();
 app.use(_express2.default.static('public'));
 
 app.get('/', function (req, res) {
-  return res.send((0, _renderer2.default)());
+  return res.send((0, _renderer2.default)(req));
 });
 
 app.listen(3000, function () {
@@ -163,6 +163,47 @@ var _react2 = _interopRequireDefault(_react);
 
 var _server = __webpack_require__(3);
 
+var _reactRouterDom = __webpack_require__(6);
+
+var _Routes = __webpack_require__(7);
+
+var _Routes2 = _interopRequireDefault(_Routes);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+exports.default = function (req) {
+	var content = (0, _server.renderToString)(_react2.default.createElement(
+		_reactRouterDom.StaticRouter,
+		{ location: req.path, context: {} },
+		_react2.default.createElement(_Routes2.default, null)
+	));
+
+	return "\n\t\t<html>\n\t\t\t<head></head>\n\t\t\t<body>\n\t\t\t\t<div id=\"root\">" + content + "</div>\n\t\t\t\t<script src=\"bundle.js\"></script>\n\t\t\t</body>\n\t\t</html>\n\t";
+};
+
+/***/ }),
+/* 6 */
+/***/ (function(module, exports) {
+
+module.exports = require("react-router-dom");
+
+/***/ }),
+/* 7 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+Object.defineProperty(exports, "__esModule", {
+	value: true
+});
+
+var _react = __webpack_require__(0);
+
+var _react2 = _interopRequireDefault(_react);
+
+var _reactRouterDom = __webpack_require__(6);
+
 var _Home = __webpack_require__(4);
 
 var _Home2 = _interopRequireDefault(_Home);
@@ -170,9 +211,11 @@ var _Home2 = _interopRequireDefault(_Home);
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 exports.default = function () {
-	var content = (0, _server.renderToString)(_react2.default.createElement(_Home2.default, null));
-
-	return "\n\t\t<html>\n\t\t\t<head></head>\n\t\t\t<body>\n\t\t\t\t<div id=\"root\">" + content + "</div>\n\t\t\t\t<script src=\"bundle.js\"></script>\n\t\t\t</body>\n\t\t</html>\n\t";
+	return _react2.default.createElement(
+		"div",
+		null,
+		_react2.default.createElement(_reactRouterDom.Route, { exact: true, path: "/", component: _Home2.default })
+	);
 };
 
 /***/ })

e322d72e4ac79cc6d8a0edf10f24f7e2d7019e17
Hazz Azimi <hassanazimi@users.noreply.github.com>
2019-09-10 21:28:01 +0100

    Video 31

--- server/src/client/actions/index.js
+++ server/src/client/actions/index.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 export const FETCH_USERS = 'fetch_users';
 
 export const fetchUsers = () => async dispatch => {
-	const res = await axios.get('http://react-ssr-api.herokuapp.com/users/xss');
+	const res = await axios.get('http://react-ssr-api.herokuapp.com/users');
 
 	dispatch({
 		type: FETCH_USERS,

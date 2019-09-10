015407a20fb5cb9e3333c6c69f523cd9db682473
Hazz Azimi <hassanazimi@users.noreply.github.com>
2019-09-10 13:19:37 +0100

    Video 13

--- server/webpack.server.js
+++ server/webpack.server.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const merge = require('webpack-merge');
 const baseConfig = require('./webpack.base.js');
+const webpackNodeExternals = require('webpack-node-externals');
 
 const config = {
 	// inform webpack that we are building a bundle
@@ -17,6 +18,8 @@ const config = {
 		filename: 'bundle.js',
 		path: path.resolve(__dirname, 'build')
 	},
+
+	externals: [webpackNodeExternals()],
 };
 
 module.exports = merge(baseConfig, config);
